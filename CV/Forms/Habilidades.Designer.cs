b490a10bddf1da550465b4f34ffdd8a74b732d71
MrLuisKiller <luis_bol_1998@hotmail.com>
2023-05-28 14:39:57 -0700

    Se creo el Formulario Habilidades

---
+++ CV/Forms/Habilidades.Designer.cs
@@ -0,0 +1,216 @@
+﻿using System.Windows.Forms;
+
+namespace CV.Forms
+{
+    partial class Habilidades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pHabilidades = new System.Windows.Forms.Panel();
+            this.pHabilidad = new System.Windows.Forms.Panel();
+            this.ImpHabilidad = new System.Windows.Forms.Panel();
+            this.LHabilidades = new CV.Componentes.MLKComboBox();
+            this.Eliminar = new CV.Componentes.MLKButton();
+            this.Habilidad = new CV.Componentes.MLKTextBox();
+            this.Agregar = new CV.Componentes.MLKButton();
+            this.menuStrip1.SuspendLayout();
+            this.pHabilidades.SuspendLayout();
+            this.pHabilidad.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(604, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            this.nuevoToolStripMenuItem.Click += new System.EventHandler(this.nuevoToolStripMenuItem_Click);
+            // 
+            // pHabilidades
+            // 
+            this.pHabilidades.Controls.Add(this.Eliminar);
+            this.pHabilidades.Controls.Add(this.LHabilidades);
+            this.pHabilidades.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pHabilidades.Location = new System.Drawing.Point(0, 24);
+            this.pHabilidades.Name = "pHabilidades";
+            this.pHabilidades.Size = new System.Drawing.Size(604, 40);
+            this.pHabilidades.TabIndex = 1;
+            // 
+            // pHabilidad
+            // 
+            this.pHabilidad.Controls.Add(this.Agregar);
+            this.pHabilidad.Controls.Add(this.Habilidad);
+            this.pHabilidad.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pHabilidad.Location = new System.Drawing.Point(0, 64);
+            this.pHabilidad.Name = "pHabilidad";
+            this.pHabilidad.Size = new System.Drawing.Size(604, 40);
+            this.pHabilidad.TabIndex = 2;
+            // 
+            // ImpHabilidad
+            // 
+            this.ImpHabilidad.AutoScroll = true;
+            this.ImpHabilidad.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ImpHabilidad.Location = new System.Drawing.Point(0, 104);
+            this.ImpHabilidad.Name = "ImpHabilidad";
+            this.ImpHabilidad.Size = new System.Drawing.Size(604, 297);
+            this.ImpHabilidad.TabIndex = 3;
+            // 
+            // LHabilidades
+            // 
+            this.LHabilidades.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.LHabilidades.BorderColor = System.Drawing.Color.Purple;
+            this.LHabilidades.BorderSize = 1;
+            this.LHabilidades.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.LHabilidades.ForeColor = System.Drawing.Color.DimGray;
+            this.LHabilidades.IconColor = System.Drawing.Color.MediumSlateBlue;
+            this.LHabilidades.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
+            this.LHabilidades.ListTextColor = System.Drawing.Color.DimGray;
+            this.LHabilidades.Location = new System.Drawing.Point(124, 3);
+            this.LHabilidades.MinimumSize = new System.Drawing.Size(200, 30);
+            this.LHabilidades.Name = "LHabilidades";
+            this.LHabilidades.Padding = new System.Windows.Forms.Padding(1);
+            this.LHabilidades.Size = new System.Drawing.Size(200, 30);
+            this.LHabilidades.TabIndex = 0;
+            this.LHabilidades.SelectedIndexChanged += new System.EventHandler(this.LHabilidades_SelectedIndexChanged);
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.Eliminar.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.Eliminar.BorderColor = System.Drawing.Color.Purple;
+            this.Eliminar.BorderRadius = 10;
+            this.Eliminar.BorderSize = 1;
+            this.Eliminar.Enabled = false;
+            this.Eliminar.FlatAppearance.BorderSize = 0;
+            this.Eliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Eliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Eliminar.ForeColor = System.Drawing.Color.White;
+            this.Eliminar.Location = new System.Drawing.Point(330, 3);
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Size = new System.Drawing.Size(100, 30);
+            this.Eliminar.TabIndex = 1;
+            this.Eliminar.Text = "Eliminar";
+            this.Eliminar.TextColor = System.Drawing.Color.White;
+            this.Eliminar.UseVisualStyleBackColor = false;
+            this.Eliminar.Click += new System.EventHandler(this.Eliminar_Click);
+            // 
+            // Habilidad
+            // 
+            this.Habilidad.BackColor = System.Drawing.SystemColors.Window;
+            this.Habilidad.BorderColor = System.Drawing.Color.Purple;
+            this.Habilidad.BorderFocusColor = System.Drawing.Color.MediumSlateBlue;
+            this.Habilidad.BorderRadius = 10;
+            this.Habilidad.BorderSize = 1;
+            this.Habilidad.Enabled = false;
+            this.Habilidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Habilidad.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Habilidad.Location = new System.Drawing.Point(124, 7);
+            this.Habilidad.Margin = new System.Windows.Forms.Padding(4);
+            this.Habilidad.Multiline = false;
+            this.Habilidad.Name = "Habilidad";
+            this.Habilidad.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.Habilidad.PasswordChar = false;
+            this.Habilidad.PlaceholderColor = System.Drawing.Color.Purple;
+            this.Habilidad.PlaceholderText = "";
+            this.Habilidad.PlaceholderTextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.Habilidad.Size = new System.Drawing.Size(200, 31);
+            this.Habilidad.TabIndex = 0;
+            this.Habilidad.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.Habilidad.Texts = "";
+            this.Habilidad.UnderlinedStyle = false;
+            this.Habilidad.TextChanged += new System.EventHandler(this.Habilidad_TextChanged);
+            this.Habilidad.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Habilidad_KeyDown);
+            // 
+            // Agregar
+            // 
+            this.Agregar.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.Agregar.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.Agregar.BorderColor = System.Drawing.Color.Purple;
+            this.Agregar.BorderRadius = 10;
+            this.Agregar.BorderSize = 1;
+            this.Agregar.Enabled = false;
+            this.Agregar.FlatAppearance.BorderSize = 0;
+            this.Agregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Agregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Agregar.ForeColor = System.Drawing.Color.White;
+            this.Agregar.Location = new System.Drawing.Point(330, 7);
+            this.Agregar.Name = "Agregar";
+            this.Agregar.Size = new System.Drawing.Size(100, 30);
+            this.Agregar.TabIndex = 1;
+            this.Agregar.Text = "Agregar";
+            this.Agregar.TextColor = System.Drawing.Color.White;
+            this.Agregar.UseVisualStyleBackColor = false;
+            this.Agregar.Click += new System.EventHandler(this.Agregar_Click);
+            // 
+            // Habilidades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 401);
+            this.Controls.Add(this.ImpHabilidad);
+            this.Controls.Add(this.pHabilidad);
+            this.Controls.Add(this.pHabilidades);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Habilidades";
+            this.Text = "Habilidades";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.pHabilidades.ResumeLayout(false);
+            this.pHabilidad.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem nuevoToolStripMenuItem;
+        private Panel pHabilidades;
+        private Componentes.MLKButton Eliminar;
+        private Componentes.MLKComboBox LHabilidades;
+        private Panel pHabilidad;
+        private Componentes.MLKButton Agregar;
+        private Componentes.MLKTextBox Habilidad;
+        private Panel ImpHabilidad;
+    }
+}
---
+++ CV/Forms/Habilidades.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CV.Forms
+{
+    public partial class Habilidades : Form
+    {
+        private Principal Principal;
+        private String Key;
+        private Dictionary<String, List<String>> H = new Dictionary<string, List<string>>();
+        
+        public Habilidades(Form F, Object X)
+        {
+            InitializeComponent();
+            Principal = F as Principal;
+            H = (Dictionary<String, List<String>>)X;
+            foreach (String Key in H.Keys)
+                LHabilidades.Items.Add(Key);
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using(Nuevo N = new Nuevo())
+            {
+                bool Error = false;
+                N.StartPosition = FormStartPosition.CenterScreen;
+                do
+                {
+                    try
+                    {
+                        switch (N.ShowDialog())
+                        {
+                            case DialogResult.OK:
+                                H.Add(N.Nombre.Texts, new List<string>());
+                                LHabilidades.Items.Add(N.Nombre.Texts);
+                                LHabilidades.SelectedIndex = LHabilidades.Items.Count - 1;
+                                break;
+                            case DialogResult.Cancel:
+                                N.Close();
+                                break;
+                        }
+                        Error = false;
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Esta habilidad ya esta en la lista", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        N.Nombre.Clear();
+                        N.Nombre.Focus();
+                        Error = true;
+                    }
+                }
+                while (Error);
+            }
+        }
+
+        private void LHabilidades_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Key = LHabilidades.Text;
+            Eliminar.Enabled = LHabilidades.SelectedIndex != -1;
+            Habilidad.Enabled = LHabilidades.SelectedIndex != -1;
+            if (LHabilidades.SelectedIndex != -1)
+                Cargar();
+        }
+
+        private async void Eliminar_Click(object sender, EventArgs e)
+        {
+            H.Remove(Key);
+            LHabilidades.Items.Remove(Key);
+            await Principal.GuardarJSON("Habilidades", H);
+            LHabilidades.SelectedIndex = LHabilidades.Items.Count - 1;
+        }
+
+        private void Habilidad_TextChanged(object sender, EventArgs e) => Agregar.Enabled = !String.IsNullOrWhiteSpace(Habilidad.Texts);
+
+        private void Habilidad_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                if (Eliminar.Enabled)
+                {
+                    AgregarH();
+                    Habilidad.Focus();
+                }
+            }
+        }
+
+        private void Agregar_Click(Object sender, EventArgs e) => AgregarH();
+
+        private void Cargar()
+        {
+            for (int i = ImpHabilidad.Controls.Count - 1; i >= 0; i--)
+                ImpHabilidad.Controls[i].Dispose();
+            foreach (String H in H[Key])
+                CrearLabel(H);
+        }
+
+        private void CrearLabel(String H)
+        {
+            LinkLabel Temp = new LinkLabel();
+            Temp.Text = H;
+            Temp.AutoSize = true;
+            Temp.Font = new Font(Font.Name, 9.5F, FontStyle.Regular);
+            Temp.LinkBehavior = LinkBehavior.NeverUnderline;
+            Temp.LinkColor = Color.Black;
+            Temp.Location = new Point(3, 3);
+            Temp.LinkClicked += new LinkLabelLinkClickedEventHandler(EliminarLabel);
+            ImpHabilidad.Controls.Add(Temp);
+            AcomodarLabel();
+        }
+
+        private async void EliminarLabel(object sender, EventArgs e)
+        {
+            LinkLabel clickedLabel = (LinkLabel)sender;
+            H[Key].Remove(clickedLabel.Text);
+            clickedLabel.Dispose();
+            AcomodarLabel();
+            await Principal.GuardarJSON("Habilidades", H);
+        }
+
+        private void AcomodarLabel()
+        {
+            int SumX = 3, SumY = 3;
+            foreach (LinkLabel label in ImpHabilidad.Controls.OfType<LinkLabel>())
+            {
+                if (SumX + label.Width > ImpHabilidad.Width)
+                {
+                    SumX = 3;
+                    SumY += 19;
+                }
+                label.Location = new Point(SumX, SumY);
+                SumX += label.Width;
+            }
+        }
+
+        private async void AgregarH()
+        {
+            if (H[Key].Contains(Habilidad.Texts))
+                MessageBox.Show($"{Habilidad.Texts} ya esta agregado en {Key}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+            {
+                CrearLabel(Habilidad.Texts);
+                H[Key].Add(Habilidad.Texts);
+                await Principal.GuardarJSON("Habilidades", H);
+                Habilidad.Clear();
+            }
+        }
+    }
+}
