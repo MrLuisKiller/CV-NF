fec70472be71a42c34ef0bb90ae21ca08ef6ab8b
MrLuisKiller <luis_bol_1998@hotmail.com>
2023-05-28 11:22:49 -0700

    Se creo el Formulario Nuevo

---
+++ CV/Forms/Nuevo.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace CV.Forms
+{
+    partial class Nuevo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Nombre = new CV.Componentes.MLKTextBox();
+            this.Cancelar = new CV.Componentes.MLKButton();
+            this.Aceptar = new CV.Componentes.MLKButton();
+            this.SuspendLayout();
+            // 
+            // Nombre
+            // 
+            this.Nombre.BackColor = System.Drawing.SystemColors.Window;
+            this.Nombre.BorderColor = System.Drawing.Color.Purple;
+            this.Nombre.BorderFocusColor = System.Drawing.Color.MediumSlateBlue;
+            this.Nombre.BorderRadius = 10;
+            this.Nombre.BorderSize = 1;
+            this.Nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Nombre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Nombre.Location = new System.Drawing.Point(13, 13);
+            this.Nombre.Margin = new System.Windows.Forms.Padding(4);
+            this.Nombre.Multiline = false;
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.Nombre.PasswordChar = false;
+            this.Nombre.PlaceholderColor = System.Drawing.Color.Purple;
+            this.Nombre.PlaceholderText = "Nombre";
+            this.Nombre.PlaceholderTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Nombre.Size = new System.Drawing.Size(204, 31);
+            this.Nombre.TabIndex = 0;
+            this.Nombre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Nombre.Texts = "";
+            this.Nombre.UnderlinedStyle = false;
+            this.Nombre.TextChanged += new System.EventHandler(this.Nombre_TextChanged);
+            this.Nombre.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Nombre_KeyDown);
+            // 
+            // Cancelar
+            // 
+            this.Cancelar.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.Cancelar.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.Cancelar.BorderColor = System.Drawing.Color.Purple;
+            this.Cancelar.BorderRadius = 10;
+            this.Cancelar.BorderSize = 1;
+            this.Cancelar.FlatAppearance.BorderSize = 0;
+            this.Cancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Cancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Cancelar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Cancelar.Location = new System.Drawing.Point(12, 51);
+            this.Cancelar.Name = "Cancelar";
+            this.Cancelar.Size = new System.Drawing.Size(100, 30);
+            this.Cancelar.TabIndex = 1;
+            this.Cancelar.Text = "Cancelar";
+            this.Cancelar.TextColor = System.Drawing.SystemColors.ControlText;
+            this.Cancelar.UseVisualStyleBackColor = false;
+            this.Cancelar.Click += new System.EventHandler(this.Cancelar_Click);
+            // 
+            // Aceptar
+            // 
+            this.Aceptar.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.Aceptar.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.Aceptar.BorderColor = System.Drawing.Color.Purple;
+            this.Aceptar.BorderRadius = 10;
+            this.Aceptar.BorderSize = 1;
+            this.Aceptar.Enabled = false;
+            this.Aceptar.FlatAppearance.BorderSize = 0;
+            this.Aceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Aceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Aceptar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Aceptar.Location = new System.Drawing.Point(118, 51);
+            this.Aceptar.Name = "Aceptar";
+            this.Aceptar.Size = new System.Drawing.Size(100, 30);
+            this.Aceptar.TabIndex = 2;
+            this.Aceptar.Text = "Aceptar";
+            this.Aceptar.TextColor = System.Drawing.SystemColors.ControlText;
+            this.Aceptar.UseVisualStyleBackColor = false;
+            this.Aceptar.Click += new System.EventHandler(this.Aceptar_Click);
+            // 
+            // Nuevo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 96);
+            this.Controls.Add(this.Aceptar);
+            this.Controls.Add(this.Cancelar);
+            this.Controls.Add(this.Nombre);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Nuevo";
+            this.Text = "Nuevo";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        public Componentes.MLKTextBox Nombre;
+        private Componentes.MLKButton Cancelar;
+        private Componentes.MLKButton Aceptar;
+    }
+}
---
+++ CV/Forms/Nuevo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CV.Forms
+{
+    public partial class Nuevo : Form
+    {
+        public Nuevo()
+        {
+            InitializeComponent();
+        }
+
+        private void Nombre_TextChanged(object sender, EventArgs e) => Aceptar.Enabled = !String.IsNullOrWhiteSpace(Nombre.Texts);
+
+        private void Nombre_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                if (Aceptar.Enabled)
+                    Aceptar.PerformClick();
+            }
+        }
+
+        private void Aceptar_Click(object sender, EventArgs e) => DialogResult = DialogResult.OK;
+
+        private void Cancelar_Click(Object sender, EventArgs e) => DialogResult = DialogResult.Cancel;
+    }
+}
