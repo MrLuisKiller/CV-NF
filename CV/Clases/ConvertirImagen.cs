34fc20495bc86a0ce510de0c4e0a71c2928335c6
MrLuisKiller <luis_bol_1998@hotmail.com>
2023-05-28 01:11:15 -0700

    Se creo la clase de Convertir Imagen

---
+++ CV/Clases/ConvertirImagen.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+namespace CV.Clases
+{
+    public class ConvertirImagen
+    {
+        public Byte[] ImagenAByte(Image img)
+        {
+            MemoryStream MS = new MemoryStream();
+            img.Save(MS, ImageFormat.Png);
+            return MS.ToArray();
+        }
+
+        public Image ByteAImagen(Byte[] ByteArray)
+        {
+            MemoryStream MS = new MemoryStream(ByteArray);
+            return Image.FromStream(MS);
+        }
+
+        public object Dash(int OP, String X)
+        {
+            switch (X)
+            {
+                case "Dash":
+                    return DashStyle.Dash;
+                case "Dot":
+                    return DashStyle.Dot;
+                case "DashDot":
+                    return DashStyle.DashDot;
+                case "DashDotDot":
+                    return DashStyle.DashDotDot;
+                case "Round":
+                    return DashCap.Round;
+                case "Triangle":
+                    return DashCap.Triangle;
+                default:
+                    if (OP == 1)
+                        return DashStyle.Solid;
+                    else
+                        return DashCap.Flat;
+            }
+        }
+    }
+}
--- CV/Form1.Designer.cs
+++ CV/Form1.Designer.cs
@@ -28,173 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.mlkButton1 = new CV.Componentes.MLKButton();
-            this.mlkCheckBox1 = new CV.Componentes.MLKCheckBox();
-            this.mlkComboBox1 = new CV.Componentes.MLKComboBox();
-            this.mlkDateTimePicker1 = new CV.Componentes.MLKDateTimePicker();
-            this.mlkMaskedTextBox1 = new CV.Componentes.MLKMaskedTextBox();
-            this.mlkPictureBox1 = new CV.Componentes.MLKPictureBox();
-            this.mlkTextBox1 = new CV.Componentes.MLKTextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.mlkPictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // mlkButton1
-            // 
-            this.mlkButton1.BackColor = System.Drawing.Color.MediumSlateBlue;
-            this.mlkButton1.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
-            this.mlkButton1.BorderColor = System.Drawing.Color.Purple;
-            this.mlkButton1.BorderRadius = 10;
-            this.mlkButton1.BorderSize = 1;
-            this.mlkButton1.FlatAppearance.BorderSize = 0;
-            this.mlkButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.mlkButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
-            this.mlkButton1.ForeColor = System.Drawing.Color.White;
-            this.mlkButton1.Location = new System.Drawing.Point(41, 39);
-            this.mlkButton1.Name = "mlkButton1";
-            this.mlkButton1.Size = new System.Drawing.Size(100, 30);
-            this.mlkButton1.TabIndex = 0;
-            this.mlkButton1.Text = "mlkButton1";
-            this.mlkButton1.TextColor = System.Drawing.Color.White;
-            this.mlkButton1.UseVisualStyleBackColor = false;
-            // 
-            // mlkCheckBox1
-            // 
-            this.mlkCheckBox1.AutoSize = true;
-            this.mlkCheckBox1.CheckedColor = System.Drawing.Color.Purple;
-            this.mlkCheckBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
-            this.mlkCheckBox1.Location = new System.Drawing.Point(178, 38);
-            this.mlkCheckBox1.MinimumSize = new System.Drawing.Size(20, 30);
-            this.mlkCheckBox1.Name = "mlkCheckBox1";
-            this.mlkCheckBox1.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.mlkCheckBox1.Size = new System.Drawing.Size(125, 30);
-            this.mlkCheckBox1.TabIndex = 1;
-            this.mlkCheckBox1.Text = "mlkCheckBox1";
-            this.mlkCheckBox1.UnCheckedColor = System.Drawing.Color.Gray;
-            this.mlkCheckBox1.UseVisualStyleBackColor = true;
-            // 
-            // mlkComboBox1
-            // 
-            this.mlkComboBox1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.mlkComboBox1.BorderColor = System.Drawing.Color.Purple;
-            this.mlkComboBox1.BorderSize = 1;
-            this.mlkComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
-            this.mlkComboBox1.ForeColor = System.Drawing.Color.DimGray;
-            this.mlkComboBox1.IconColor = System.Drawing.Color.MediumSlateBlue;
-            this.mlkComboBox1.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
-            this.mlkComboBox1.ListTextColor = System.Drawing.Color.DimGray;
-            this.mlkComboBox1.Location = new System.Drawing.Point(41, 103);
-            this.mlkComboBox1.MinimumSize = new System.Drawing.Size(200, 30);
-            this.mlkComboBox1.Name = "mlkComboBox1";
-            this.mlkComboBox1.Padding = new System.Windows.Forms.Padding(1);
-            this.mlkComboBox1.Size = new System.Drawing.Size(200, 30);
-            this.mlkComboBox1.TabIndex = 2;
-            // 
-            // mlkDateTimePicker1
-            // 
-            this.mlkDateTimePicker1.BorderColor = System.Drawing.Color.Purple;
-            this.mlkDateTimePicker1.BorderSize = 1;
-            this.mlkDateTimePicker1.CustomFormat = "dd/MM/yyyy";
-            this.mlkDateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
-            this.mlkDateTimePicker1.Location = new System.Drawing.Point(68, 169);
-            this.mlkDateTimePicker1.MinimumSize = new System.Drawing.Size(0, 30);
-            this.mlkDateTimePicker1.Name = "mlkDateTimePicker1";
-            this.mlkDateTimePicker1.Size = new System.Drawing.Size(200, 30);
-            this.mlkDateTimePicker1.SkinColor = System.Drawing.Color.MediumSlateBlue;
-            this.mlkDateTimePicker1.TabIndex = 3;
-            this.mlkDateTimePicker1.TextColor = System.Drawing.Color.White;
-            // 
-            // mlkMaskedTextBox1
-            // 
-            this.mlkMaskedTextBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.mlkMaskedTextBox1.BorderColor = System.Drawing.Color.Purple;
-            this.mlkMaskedTextBox1.BorderFocusColor = System.Drawing.Color.MediumSlateBlue;
-            this.mlkMaskedTextBox1.BorderRadius = 10;
-            this.mlkMaskedTextBox1.BorderSize = 1;
-            this.mlkMaskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
-            this.mlkMaskedTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.mlkMaskedTextBox1.Location = new System.Drawing.Point(68, 229);
-            this.mlkMaskedTextBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.mlkMaskedTextBox1.Mask = null;
-            this.mlkMaskedTextBox1.Name = "mlkMaskedTextBox1";
-            this.mlkMaskedTextBox1.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
-            this.mlkMaskedTextBox1.PasswordChar = false;
-            this.mlkMaskedTextBox1.PlaceholderColor = System.Drawing.Color.Purple;
-            this.mlkMaskedTextBox1.PlaceholderText = "";
-            this.mlkMaskedTextBox1.PlaceholderTextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.mlkMaskedTextBox1.Size = new System.Drawing.Size(200, 30);
-            this.mlkMaskedTextBox1.TabIndex = 4;
-            this.mlkMaskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.mlkMaskedTextBox1.Texts = "";
-            this.mlkMaskedTextBox1.UnderlinedStyle = false;
-            // 
-            // mlkPictureBox1
-            // 
-            this.mlkPictureBox1.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
-            this.mlkPictureBox1.BorderColor1 = System.Drawing.Color.HotPink;
-            this.mlkPictureBox1.BorderColor2 = System.Drawing.Color.Purple;
-            this.mlkPictureBox1.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
-            this.mlkPictureBox1.BorderSize = 2;
-            this.mlkPictureBox1.GradientAngle = 50F;
-            this.mlkPictureBox1.Location = new System.Drawing.Point(68, 288);
-            this.mlkPictureBox1.Name = "mlkPictureBox1";
-            this.mlkPictureBox1.Size = new System.Drawing.Size(100, 100);
-            this.mlkPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.mlkPictureBox1.TabIndex = 5;
-            this.mlkPictureBox1.TabStop = false;
-            // 
-            // mlkTextBox1
-            // 
-            this.mlkTextBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.mlkTextBox1.BorderColor = System.Drawing.Color.Purple;
-            this.mlkTextBox1.BorderFocusColor = System.Drawing.Color.MediumSlateBlue;
-            this.mlkTextBox1.BorderRadius = 10;
-            this.mlkTextBox1.BorderSize = 1;
-            this.mlkTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
-            this.mlkTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.mlkTextBox1.Location = new System.Drawing.Point(394, 168);
-            this.mlkTextBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.mlkTextBox1.Multiline = false;
-            this.mlkTextBox1.Name = "mlkTextBox1";
-            this.mlkTextBox1.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
-            this.mlkTextBox1.PasswordChar = false;
-            this.mlkTextBox1.PlaceholderColor = System.Drawing.Color.Purple;
-            this.mlkTextBox1.PlaceholderText = "";
-            this.mlkTextBox1.PlaceholderTextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.mlkTextBox1.Size = new System.Drawing.Size(200, 31);
-            this.mlkTextBox1.TabIndex = 6;
-            this.mlkTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.mlkTextBox1.Texts = "";
-            this.mlkTextBox1.UnderlinedStyle = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.mlkTextBox1);
-            this.Controls.Add(this.mlkPictureBox1);
-            this.Controls.Add(this.mlkMaskedTextBox1);
-            this.Controls.Add(this.mlkDateTimePicker1);
-            this.Controls.Add(this.mlkComboBox1);
-            this.Controls.Add(this.mlkCheckBox1);
-            this.Controls.Add(this.mlkButton1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.mlkPictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private Componentes.MLKButton mlkButton1;
-        private Componentes.MLKCheckBox mlkCheckBox1;
-        private Componentes.MLKComboBox mlkComboBox1;
-        private Componentes.MLKDateTimePicker mlkDateTimePicker1;
-        private Componentes.MLKMaskedTextBox mlkMaskedTextBox1;
-        private Componentes.MLKPictureBox mlkPictureBox1;
-        private Componentes.MLKTextBox mlkTextBox1;
     }
 }
 
